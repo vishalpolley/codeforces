37fc60495d18b9f394bd9a2cc97fae051380cd11
vishalpolley <vishalpolley290996@gmail.com>
2017-10-22 11:47:55 +0530

    Updated solution to problem 472A.

--- 472A.cpp
+++ 472A.cpp
@@ -1,15 +1,71 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+bool is_prime(int n)
+{
+    if(n==2)
+        return true;
+    if(n%2==0)
+        return false;
+    for(int i=3; i*i<=n; i+=2)
+    {
+        if(n%i==0)
+            return false;
+    }
+    return true;
+}
+
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int n;
+    int n, x, y, z;
     cin >> n;
+    bool flag=false;
     if(n%2==0)
-        cout << 8 << " " << n-8;
+    {
+        x=n/2;
+        y=x-1;
+        z=x+1;
+        while(flag!=true)
+        {
+            if(!is_prime(x))
+            {
+                cout << x << " " << x;
+                flag=true;
+            }
+            else
+            {
+                if(!is_prime(y)&&!is_prime(z))
+                {
+                     cout << y << " " << z;
+                     flag=true;
+                }
+                else
+                {
+                    y--;
+                    z++;
+                }
+            }
+        }
+    }
     else
-        cout << 9 << " " << n-9;
+    {
+        y=(n+1)/2;
+        z=(n-1)/2;
+        while(flag!=true)
+        {
+            if(!is_prime(y)&&!is_prime(z))
+            {
+                cout << y << " " << z;
+                flag=true;
+            }
+            else
+            {
+                y++;
+                z--;
+            }
+        }
+    }
     return 0;
 }
